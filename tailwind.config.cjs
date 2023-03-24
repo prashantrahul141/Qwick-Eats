7231fd8237609c4fae1258d79cd8e43788f7ba9d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-24 15:50:12 +0530

    add: colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 const config = {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  content: ['./src/**/*.{js,ts,jsx,tsx}'],
   future: {
     hoverOnlyWhenSupported: true,
   },
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bg: '#121313',
+        bord: '#192a2a',
+        'bord-act': '#f0f0f0',
+        main: '#f0f0f0',
+        muted: 'rgb(147,154,154)',
+        disabled: '#1c1d1d',
+      },
+    },
   },
   plugins: [],
 };
