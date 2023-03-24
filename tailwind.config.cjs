29c18890fb14888ea6c623b9835ac62bc1653f2d
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-24 13:56:44 +0530

    initial commmit

---
+++ tailwind.config.cjs
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+const config = {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+  future: {
+    hoverOnlyWhenSupported: true,
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+module.exports = config;
