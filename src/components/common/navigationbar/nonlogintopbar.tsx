a68f3d9bd6b4145ddfc71278635264c651001167
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-24 20:14:46 +0530

    style: better styling for mobile devices

--- src/components/common/navigationbar/nonlogintopbar.tsx
+++ src/components/common/navigationbar/nonlogintopbar.tsx
@@ -5,14 +5,14 @@ import Icon from '../icon';
 
 const NonLoginTopBar: FC = () => {
   return (
-    <nav className='flex h-14 w-full flex-col items-center border-b border-b-muted bg-bg-white dark:border-b-bord dark:bg-black'>
+    <nav className='flex h-16 w-full flex-col items-center border-b border-b-muted bg-bg-white dark:border-b-bord dark:bg-black sm:h-14'>
       <div className='relative flex h-full w-full max-w-4xl items-center'>
         <Link
           href='/'
-          className='absolute left-4 h-12 w-12 text-black dark:text-main'>
+          className='absolute left-4 h-16 w-16 text-black dark:text-main sm:h-14 sm:w-14'>
           <Icon></Icon>
         </Link>
-        <div className='absolute right-2 flex items-center justify-center gap-2'>
+        <div className='absolute right-2 flex items-center justify-center gap-4 sm:gap-2'>
           <Link
             href={'/signin'}
             className='text-gray-600 hover:text-black dark:text-muted dark:hover:text-main'>
