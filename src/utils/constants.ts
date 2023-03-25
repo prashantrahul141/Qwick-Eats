e5b8f482cc74cf8a7dc231745c1a66792ee81bf2
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-25 19:58:25 +0530

    add: constants file, random address

---
+++ src/utils/constants.ts
@@ -0,0 +1,10 @@
+const randomAddress = [
+  '4040/4041, Chandivali Farm Road, Andheri(e), Mumbai',
+  '38, Bagh Lingampally, Hyderabad',
+  '18/48, Patel Nagar (e), Delhi',
+  '22-7-238/a, Chatta Bazar, Hyderabad',
+  'B-1, Bhagwati Chambers, Paldi, Ahmedabad',
+  "31, Judge's Court Road, Alipore, Kolkata",
+] as const;
+
+export { randomAddress };
