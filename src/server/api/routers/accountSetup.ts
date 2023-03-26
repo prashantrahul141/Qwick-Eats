fae993caadf625ccf0594ce1823b6b19b9c7959f
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-26 17:22:59 +0530

    account setup route

---
+++ src/server/api/routers/accountSetup.ts
@@ -0,0 +1,33 @@
+import { z } from 'zod';
+
+import { createTRPCRouter, protectedProcedure } from '@server/api/trpc';
+import { prisma } from '@src/server/db';
+
+export const accountSetupRouter = createTRPCRouter({
+  setupCustomer: protectedProcedure
+    .input(z.object({ addr: z.string(), phoneNumber: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const user = await prisma.user.findUnique({
+        where: {
+          id: ctx.session.user.id,
+        },
+        select: {
+          accountSetupDone: true,
+        },
+      });
+
+      if (user && !user.accountSetupDone) {
+        await prisma.user.update({
+          where: {
+            id: ctx.session.user.id,
+          },
+          data: {
+            address: input.addr,
+            phoneNumber: input.phoneNumber,
+            accountSetupDone: true,
+            role: 'CUSTOMER',
+          },
+        });
+      }
+    }),
+});
