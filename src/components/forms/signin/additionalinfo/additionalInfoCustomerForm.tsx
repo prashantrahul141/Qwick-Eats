822352fc537e1df8304a6ba71ddd581516c4e635
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-26 16:56:28 +0530

    add: additional info customer form component

---
+++ src/components/forms/signin/additionalinfo/additionalInfoCustomerForm.tsx
@@ -0,0 +1,98 @@
+import InfoHoverIcon from '@src/components/common/infoHoverIcon';
+import { randomAddress } from '@src/utils/constants';
+import type { FC } from 'react';
+import type { SubmitHandler } from 'react-hook-form';
+import { useForm } from 'react-hook-form';
+
+type FormInputs = {
+  addr: string;
+  phoneNumber: number;
+};
+
+const AdditionalInfoCustomerForm: FC = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormInputs>({ mode: 'all' });
+
+  const submitCustomerForm: SubmitHandler<FormInputs> = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <form
+      className='w-full max-w-xl'
+      onSubmit={handleSubmit(submitCustomerForm)}>
+      <fieldset className='group relative mb-6 w-full max-w-xl'>
+        <label className='absolute -top-[.68rem] left-4 flex select-none items-center gap-2 bg-white text-sm text-gray-700 dark:bg-bg dark:text-gray-300'>
+          <span>Full Address</span>{' '}
+          <InfoHoverIcon infoText='Your address will be shown only to the places you order from.'></InfoHoverIcon>
+        </label>
+        <textarea
+          className='input resize-none pt-2'
+          {...register('addr', {
+            required: {
+              value: true,
+              message: 'Cannot submit without address.',
+            },
+            minLength: {
+              message: 'Cannot be shorter than 10 characters.',
+              value: 11,
+            },
+          })}
+          placeholder={
+            randomAddress[Math.floor(Math.random() * randomAddress.length)]
+          }></textarea>
+        {errors.addr && (
+          <span className='text-[#ff0f0f] dark:text-red-500'>
+            {errors.addr.message}
+          </span>
+        )}
+      </fieldset>
+
+      <fieldset className='group relative mb-6 w-full max-w-xl'>
+        <label className='absolute -top-[.68rem] left-4 flex select-none items-center gap-2 bg-white text-sm text-gray-700 dark:bg-bg dark:text-gray-300'>
+          <span>Phone number</span>
+          <InfoHoverIcon infoText='Your phone number will be shown only to the places you order from.'></InfoHoverIcon>
+        </label>
+        <input
+          type={'number'}
+          className='input resize-none pt-2'
+          {...register('phoneNumber', {
+            pattern: {
+              message: 'Invalid Phone number.',
+              value: /[0-9]/,
+            },
+            required: {
+              value: true,
+              message: 'Cannot submit without phone number.',
+            },
+            minLength: {
+              message: 'Cannot be shorter than 10 characters.',
+              value: 10,
+            },
+            maxLength: {
+              message: 'Cannot be longer than 10 characters.',
+              value: 10,
+            },
+          })}
+          placeholder={'1234567890'}></input>
+        {errors.phoneNumber && (
+          <span className='text-[#ff0f0f] dark:text-red-500'>
+            {errors.phoneNumber.message}
+          </span>
+        )}
+      </fieldset>
+      <div className='relative w-full'>
+        <button
+          type='submit'
+          className='btn absolute right-1 w-fit py-[.4rem] px-2'>
+          Start Ordering
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default AdditionalInfoCustomerForm;
