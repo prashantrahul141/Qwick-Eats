8814934a0b49b1daaca89e036bde2e539173635a
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-26 18:36:18 +0530

    add: customer only guards

---
+++ src/components/guards/customerOnlyGuard.tsx
@@ -0,0 +1,44 @@
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import type { FC, ReactNode } from 'react';
+import LoadingSpinner from '../common/loadingSpinner';
+
+const CustomerOnlyGuard: FC<{ children: ReactNode }> = ({ children }) => {
+  const { data: session, status } = useSession();
+  const router = useRouter();
+
+  if (status === 'loading') {
+    return (
+      <div className='h-full w-full'>
+        <LoadingSpinner></LoadingSpinner>
+      </div>
+    );
+  }
+
+  if (status === 'unauthenticated') {
+    void router.push('/signin');
+  }
+
+  if (status === 'authenticated' && session) {
+    if (session.user.role === 'VENDOR') {
+      void router.push('/sell');
+    } else if (session.user.role === 'ADMIN') {
+      void router.push('/dashboard');
+    } else if (session.user.role === 'NOTDEFINED') {
+      void router.push('/signin/new');
+    }
+
+    if (session.user.role !== 'CUSTOMER') {
+      return <></>;
+    }
+
+    return <>{children}</>;
+  }
+  return (
+    <div className='h-full w-full'>
+      <LoadingSpinner></LoadingSpinner>
+    </div>
+  );
+};
+
+export default CustomerOnlyGuard;
