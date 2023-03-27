1e874f26e59a37dcd5000a70c3f0c7a62151148c
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-28 03:11:19 +0530

    add: active tab prop

--- src/components/common/navigationbar/TopBarResponsive.tsx
+++ src/components/common/navigationbar/TopBarResponsive.tsx
@@ -1,18 +1,23 @@
-import type { DefinedUserRole } from '@src/types';
+import type { DefinedUserRole, tabOptionsNames } from '@src/types';
 import type { FC } from 'react';
 import CustomerTopBarLargeScreen from './TopBarLargeScreen';
 import CustomerTopBarMobile from './TopBarMobile';
 
 const CustomerTopBarResponsive: FC<{
   type: DefinedUserRole;
-}> = ({ type }) => {
+  activeTab?: tabOptionsNames;
+}> = ({ type, activeTab = '' }) => {
   return (
     <div className='w-full'>
       <div className='sm:hidden'>
-        <CustomerTopBarMobile type={type}></CustomerTopBarMobile>
+        <CustomerTopBarMobile
+          type={type}
+          activeTab={activeTab}></CustomerTopBarMobile>
       </div>
       <div className='hidden sm:block'>
-        <CustomerTopBarLargeScreen type={type}></CustomerTopBarLargeScreen>
+        <CustomerTopBarLargeScreen
+          type={type}
+          activeTab={activeTab}></CustomerTopBarLargeScreen>
       </div>
     </div>
   );
