324a944855c84ae3e2a3440b856c203cfcf4e16b
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-03-27 14:13:42 +0530

    add: host for images

--- next.config.mjs
+++ next.config.mjs
@@ -12,5 +12,46 @@ const config = {
     locales: ['en'],
     defaultLocale: 'en',
   },
+  images: {
+    domains: [
+      'cdn.discordapp.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+      'media.discordapp.net',
+      'res.cloudinary.com',
+    ],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lh3.googleusercontent.com',
+        port: '',
+        pathname: '/a/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'cdn.discordapp.com',
+        port: '',
+        pathname: '/avatars/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'media.discordapp.net',
+        port: '',
+        pathname: '/attachments/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+        port: '',
+        pathname: '/u/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 export default config;
