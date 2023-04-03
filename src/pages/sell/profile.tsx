00ed36bde33402da15f1f9c390e6593e7efde7bf
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-04-03 22:46:09 +0530

    add: vendor profile page

--- src/pages/sell/profile.tsx
+++ src/pages/sell/profile.tsx
@@ -1,4 +1,5 @@
 import TopBarResponsive from '@src/components/common/navigationbar/TopBarResponsive';
+import VendorEditProfileForm from '@src/components/forms/editProfile/vendorEditProfile';
 import VendorOnlyGuard from '@src/components/guards/vendorOnlyGuard';
 import type { FC } from 'react';
 
@@ -6,6 +7,14 @@ const VendorProfilePage: FC = () => {
   return (
     <VendorOnlyGuard>
       <TopBarResponsive type={'VENDOR'} activeTab='Profile'></TopBarResponsive>
+      <div className='flex w-full justify-center'>
+        <div className='w-full max-w-5xl px-4 py-4'>
+          <span className='text-3xl font-semibold text-black dark:text-white'>
+            Profile
+          </span>
+          <VendorEditProfileForm></VendorEditProfileForm>
+        </div>
+      </div>
     </VendorOnlyGuard>
   );
 };
