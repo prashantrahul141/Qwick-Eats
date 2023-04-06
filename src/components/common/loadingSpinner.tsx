f400ed10df97a882e5b9f3d1d2a6ce6dacecf4c0
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-04-06 17:45:26 +0530

    style: decrease size of loading spinner

--- src/components/common/loadingSpinner.tsx
+++ src/components/common/loadingSpinner.tsx
@@ -3,7 +3,7 @@ import type { FC } from 'react';
 const LoadingSpinner: FC = () => {
   return (
     <div className='flex h-full w-full items-center justify-center'>
-      <div className='h-8 w-8'>
+      <div className='h-6 w-6'>
         <div className='h-full w-full animate-spin-ease rounded-full border-[3px] border-gray-200 border-t-bg ease-in-out  dark:border-main/10 dark:border-t-white'></div>
       </div>
     </div>
