d2d59b2e33e290ea76a1d8ecc51b82ae00f3185c
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-04-06 16:20:59 +0530

    add: allOrderFilterOptions to constants

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,3 +1,4 @@
+import type { orderState } from '@prisma/client';
 import { z } from 'zod';
 
 const randomAddress = [
@@ -15,4 +16,15 @@ const cloudinaryRequiredFieldsSchema = z.object({
   signature: z.string(),
 });
 
-export { randomAddress, cloudinaryRequiredFieldsSchema };
+const allOrderFilterOptions: Array<{
+  title: string;
+  value: orderState | 'ALL';
+}> = [
+  { title: 'All', value: 'ALL' },
+  { title: 'Cancelled', value: 'CANCELLED' },
+  { title: 'Done', value: 'DONE' },
+  { title: 'Pending', value: 'PENDING' },
+  { title: 'Processing', value: 'PROCESSSING' },
+];
+
+export { randomAddress, cloudinaryRequiredFieldsSchema, allOrderFilterOptions };
