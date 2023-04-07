0ee4920e05baab5afd65a6f0530a2cbc812bd854
Prashant <59825803+prashantrahul141@users.noreply.github.com>
2023-04-07 16:10:45 +0530

    feat: update order state

--- src/components/orders/vendor/vendorOrder.tsx
+++ src/components/orders/vendor/vendorOrder.tsx
@@ -1,10 +1,35 @@
 import type { TReturnVendorOrder } from '@src/types';
+import { getOrderStatusColor } from '@src/utils/clientSideUtilFunctions';
 import type { FC } from 'react';
+import { useState, Fragment } from 'react';
+import { Listbox, Transition } from '@headlessui/react';
+import type { orderState } from '@prisma/client';
+import { BsChevronExpand } from 'react-icons/bs';
+import { orderStateOptions } from '@src/utils/constants';
+import { api } from '@src/utils/api';
 
 const VendorOrder: FC<{ order: TReturnVendorOrder }> = ({ order }) => {
+  const [defaultOrderState, setDefaultOrderState] = useState<orderState>(
+    order.orderState
+  );
+  const [orderStateVar, setOrderStateVar] = useState<orderState>(
+    order.orderState
+  );
+  const updateOrderStateQuery = api.vendorOrder.updateOrderState.useMutation();
+
+  const updateOrderState = async () => {
+    await updateOrderStateQuery.mutateAsync({
+      targetOrderId: order.id,
+      targetState: orderStateVar,
+    });
+
+    setDefaultOrderState(orderStateVar);
+  };
+
   return (
     <>
       <div className='rounded-md border border-gray-300 px-3 py-2 text-black shadow-md dark:border-bord dark:bg-black dark:text-white'>
+        {/* primary info */}
         <div className='flex justify-center gap-4'>
           <div className='w-1/2'>
             <div className=''>
@@ -42,6 +67,75 @@ const VendorOrder: FC<{ order: TReturnVendorOrder }> = ({ order }) => {
               </address>
             </div>
           </div>
+        </div>
+
+        {/* status */}
+        <div className='mt-1'>
+          <span className='text-xs'>Status:&nbsp;</span>
+          <span className={getOrderStatusColor(defaultOrderState) + ''}>
+            {defaultOrderState}
+          </span>
+        </div>
+
+        {/* interactions */}
+        <div className='mt-3 flex items-center justify-center gap-3'>
+          <Listbox value={orderStateVar} onChange={setOrderStateVar}>
+            <div className='relative w-[10rem] select-none'>
+              <Listbox.Button className='relative w-full cursor-pointer rounded-md border border-muted/20 bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-300 dark:border-bord dark:bg-black dark:text-white sm:text-sm'>
+                <span className='block truncate'>
+                  {`${String(orderStateVar[0])}${String(
+                    orderStateVar.slice(1, orderStateVar.length).toLowerCase()
+                  )}`}
+                </span>
+                <span className='pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2'>
+                  <BsChevronExpand
+                    className='h-5 w-5 text-gray-400'
+                    aria-hidden='true'
+                  />
+                </span>
+              </Listbox.Button>
+              <Transition
+                as={Fragment}
+                leave='transition ease-in duration-100'
+                leaveFrom='opacity-100'
+                leaveTo='opacity-0'>
+                <Listbox.Options className='absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none dark:bg-black sm:text-sm'>
+                  {orderStateOptions.map(
+                    (orderStateOption, orderStateOptionIndex) => (
+                      <Listbox.Option
+                        key={orderStateOptionIndex}
+                        className={({ active }) =>
+                          `relative cursor-pointer select-none py-2 pl-10 pr-4 ${
+                            active
+                              ? 'bg-bord text-white'
+                              : 'text-gray-600 dark:text-gray-300'
+                          }`
+                        }
+                        value={orderStateOption.value}>
+                        {(selected) => (
+                          <>
+                            <span
+                              className={`block truncate ${
+                                selected ? 'font-medium' : 'font-normal'
+                              }`}>
+                              {orderStateOption.title}
+                            </span>
+                          </>
+                        )}
+                      </Listbox.Option>
+                    )
+                  )}
+                </Listbox.Options>
+              </Transition>
+            </div>
+          </Listbox>
+
+          <button
+            onClick={updateOrderState}
+            className='btn h-9 max-w-fit px-3 py-0'
+            disabled={orderStateVar === defaultOrderState}>
+            Update
+          </button>
         </div>
       </div>
     </>
